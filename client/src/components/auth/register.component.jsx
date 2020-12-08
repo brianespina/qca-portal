8f5b85aa954450bf2fa34a53c5ef51a93f451058
Brian <espinabrian@gmail.com>
2020-12-08 12:56:20 +0800

    fixed register form alignment

--- client/src/components/auth/register.component.jsx
+++ client/src/components/auth/register.component.jsx
@@ -42,49 +42,51 @@ const Register = ({ setAlert, register, isAuthenticated, loading }) => {
     }
 
     return(
-        <Card classes="card--small card--floating">
-            <Alert />
-            <h1 className="large text-primary">Sign Up</h1>
-            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-            <form className="form" onSubmit={e => handleSubmit(e)}>
-                <div className="form-group">
-                <input type="text" placeholder="Name" name="name" value={name} onChange={e => handleCange(e)} />
-                </div>
-                <div className="form-group">
-                <input type="email" placeholder="Email Address" value={email} onChange={e => handleCange(e)} name="email" />
-                <small className="form-text"
-                    >This site uses Gravatar so if you want a profile image, use a
-                    Gravatar email</small
-                >
-                </div>
-                <div className="form-group">
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                  
-                    value={password} 
-                    onChange={e => handleCange(e)}
-                />
-                </div>
-                <div className="form-group">
-                <input
-                    type="password"
-                    placeholder="Confirm Password"
-                    name="password2"
-                   
-                    value={password2} 
-                    onChange={e => handleCange(e)}
-                />
-                </div>
-                {loading 
-                ? <Loader type="ThreeDots" color="#DA3642" height={80} width={80} />
-                : <input type="submit" className="btn btn-primary" value="Register" />}	
-            </form>
-            <p className="my-1">
-                Already have an account? <Link to="/login">Sign In</Link>
-            </p>
-        </Card>
+        <div className="floating-form-wrapper">  
+            <Card classes="card--small card--floating">
+                <Alert />
+                <h1 className="large text-primary">Sign Up</h1>
+                <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+                <form className="form" onSubmit={e => handleSubmit(e)}>
+                    <div className="form-group">
+                    <input type="text" placeholder="Name" name="name" value={name} onChange={e => handleCange(e)} />
+                    </div>
+                    <div className="form-group">
+                    <input type="email" placeholder="Email Address" value={email} onChange={e => handleCange(e)} name="email" />
+                    <small className="form-text"
+                        >This site uses Gravatar so if you want a profile image, use a
+                        Gravatar email</small
+                    >
+                    </div>
+                    <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                    
+                        value={password} 
+                        onChange={e => handleCange(e)}
+                    />
+                    </div>
+                    <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                    
+                        value={password2} 
+                        onChange={e => handleCange(e)}
+                    />
+                    </div>
+                    {loading 
+                    ? <Loader type="ThreeDots" color="#DA3642" height={80} width={80} />
+                    : <input type="submit" className="btn btn-primary" value="Register" />}	
+                </form>
+                <p className="my-1">
+                    Already have an account? <Link to="/login">Sign In</Link>
+                </p>
+            </Card>
+        </div>   
     )
 }
 
