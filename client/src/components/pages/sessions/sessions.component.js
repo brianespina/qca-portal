f02cdd2649a04746e2a91da45a759fc49655d34a
Brian Espina <espinabrian@gmail.com>
2021-02-24 21:24:20 +0800

    added links for attendees

--- client/src/components/pages/sessions/sessions.component.js
+++ client/src/components/pages/sessions/sessions.component.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 import { getSessions } from '../../../redux/reducers/session/session.actions'
+import { getAllProfiles } from '../../../redux/reducers/profile/profile.actions'
 import { createStructuredSelector } from 'reselect'
 import { selectSessionItems, selectSessionIsLoading } from '../../../redux/reducers/session/session.selectors'
 import { selectAllStudents } from '../../../redux/reducers/profile/profile.selectors'
@@ -15,35 +16,46 @@ import PageTitle from '../../page-title/page-title.component'
 import LoaderTable from '../../loader-table/loader-table.component'
 
 
-const Sessions = ({ getSessions, sessions, isLoading }) => {
+const Sessions = ({ getSessions, sessions, isLoading, profiles, getAllProfiles }) => {
 
     useEffect(()=>{
         getSessions()  
+        if(!profiles.length){
+            getAllProfiles()
+        }
     }, [])
 
+    const setSessionRows = (loadState) => {
+
+        if(loadState){
+            const items = sessions.map( (item) => {
+                let attendeeNames = []
+                item.attendees.forEach( (attendee) => {
+                    if(attendee.user.name === item.coach.name) return
+                    let profile = profiles.find( prof => prof.user._id ==  attendee.user._id)
+                    attendeeNames.push(<Link class="attendees-list" to={`/profile/${profile._id}`}>{ attendee.user.name }</Link>)
+                })
+        
+                let columns = [
+                    moment(item.date).format('LL'),
+                    item.coach.name,
+                    attendeeNames
+                ]
+                return columns
+            })
+        
+            return items.map( (item, i) => <TableRow key={i} items={item} />)
+        }
+
+    }
     
     const headerItems = [
         'Date',
         'Coach',
         'Attendees'
     ]
-    
-    const items = sessions.map( (item) => {
-        let attendeeNames = []
-        item.attendees.forEach( (attendee) => {
-            if(attendee.user.name === item.coach.name) return
-            attendeeNames.push(<Link>{ attendee.user.name }</Link>)
-        })
 
-        let columns = [
-            moment(item.date).format('LL'),
-            item.coach.name,
-            attendeeNames
-        ]
-        return columns
-    })
-
-    const sessionRows = items.map( (item, i) => <TableRow key={i} items={item} />)
+    let sessionRows = setSessionRows(profiles.length)
 
     return(
         <>
@@ -70,7 +82,8 @@ const mapStateToProps = createStructuredSelector({
 })
 
 const mapDispatchToProps = {
-    getSessions
+    getSessions,
+    getAllProfiles
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Sessions)
