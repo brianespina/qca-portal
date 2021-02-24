f35e6ee95fa271eefb6aa4f3ece17688e5c07886
Brian Espina <espinabrian@gmail.com>
2021-02-24 22:31:58 +0800

    button icon move to after label

--- client/src/components/button/button.component.jsx
+++ client/src/components/button/button.component.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Button = ({ title, icon, children, ...otherProps }) =>
     <Link className="button" {...otherProps} >
-        {children} { title }
+        { title } {children}
     </Link>
 
 
